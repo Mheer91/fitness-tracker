bce1d63fed258dd5a214f91d163d36e4e0f0b585
Ham Hock <mheer91@icloud.com>
2021-10-15 16:06:09 -0500

    Update seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = require('../models');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase',
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
