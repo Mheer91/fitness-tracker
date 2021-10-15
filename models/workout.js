71a3caaeeea39c740286f70b1fa3e381c350ac48
Ham Hock <mheer91@icloud.com>
2021-10-15 17:51:54 -0500

    get routes working

--- models/workout.js
+++ models/workout.js
@@ -2,31 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Please enter the type of workout!"
+    day: {
+        type: Date,
+        default: Date.now
     },
-    name: {
-        type: String,
-        trim: true,
-        required: "Please enter workout name!"
-    },
-    duration: {
-        type: Number,
-    },
-    weight: {
-        type: Number,
-    },
-    reps: {
-        type: Number,
-    },
-    sets: {
-        type: Number,
-    },
-    distance: {
-        type: Number,
-    }
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,16 +2,23 @@ const db = require("../models/workout");
 const router = require("express").Router();
 
 
-// db.find({}).then(function (res) {
-//     console.log("Checking if db is populated");
-//     if (res.length === 0) {
-//         console.log("DB is empty");
-//         require("../seeders/seed");
-//     }
-// });
-
 router.get("/api/workouts", (req, res) => {
     db.find().then((workouts) => res.json(workouts)).catch((err) => res.json(err));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.find().then((workouts) => res.json(workouts)).catch((err) => res.json(err));
+})
+
+router.post("/api/workouts", ({ body }, res) => {
+    console.log(body)
+    db.create(body)
+    .then((workout) => {
+        res.json(workout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
 });
 
 module.exports = router;
