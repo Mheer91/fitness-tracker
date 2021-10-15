08457c180c3d5fc14e43742d4b573edf5d617c33
Ham Hock <mheer91@icloud.com>
2021-10-15 16:04:40 -0500

    Update seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = require('../models');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
