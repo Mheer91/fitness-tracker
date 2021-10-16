aaaaa438025f270ea53f9aa97e78035d00189e90
Ham Hock <mheer91@icloud.com>
2021-10-16 16:34:22 -0500

    api routing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,22 +3,55 @@ const router = require("express").Router();
 
 
 router.get("/api/workouts", (req, res) => {
-    db.find().then((workouts) => res.json(workouts)).catch((err) => res.json(err));
+    db.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+    .then((workout) => res.json(workout))
+    .catch((err) => res.json(err))
 });
 
 router.get("/api/workouts/range", (req, res) => {
-    db.find().then((workouts) => res.json(workouts)).catch((err) => res.json(err));
-})
+    db.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+    .sort({ day: -1}).limit(7)
+    .then((workout) => res.json(workout))
+    .catch((err) => res.json(err))
+});
 
 router.post("/api/workouts", (req, res) => {
-    console.log(req.body)
     db.create()
-    .then((workout) => {
-        res.json(workout);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+        .then((workout) => {
+            res.json(workout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.findByIdAndUpdate({
+        _id: req.params.id
+    },
+        {
+            $push: {
+                exercises: req.body
+            }
+        })
+        .then((update) => res.json(update))
+        .catch((err) => res.json(err))
 });
 
 module.exports = router;
